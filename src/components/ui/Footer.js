e7328a64b01c94a92d50a6d44d41db89fbeff689
morad elazyzy <moradazyzy@gmail.com>
2022-03-01 17:22:27 +0100

    adding social media links

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -6,9 +6,18 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-left">moradelazyzy@gmail.com</div>
         <div className="footer-center">
-          <i className="fa fa-github-square"></i>
-          <i className="fa fa-linkedin"></i>
-          <i className="fa fa-twitter-square"></i>
+          <a href="https://github.com/moradov" target="_blank">
+            <i className="fa fa-github-square"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/morad-elazyzy-8b12401a6/"
+            target="_blank"
+          >
+            <i className="fa fa-linkedin"></i>
+          </a>
+          <a href="https://www.behance.net/moradelazyzy" target="_blank">
+            <i className="fa fa-behance-square"></i>
+          </a>
         </div>
         <div className="footer-left">
           <p>&copy; 2021 Morad Elazyzy</p>
