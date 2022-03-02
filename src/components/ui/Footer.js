5606ac4d8a4fcb8514f125368f93dc4b537758b4
morad elazyzy <moradazyzy@gmail.com>
2022-03-02 11:43:52 +0100

    fix eslint warnings

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -6,16 +6,21 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-left">moradelazyzy@gmail.com</div>
         <div className="footer-center">
-          <a href="https://github.com/moradov" target="_blank">
+          <a href="https://github.com/moradov" target="_blank" rel="noreferrer">
             <i className="fa fa-github-square"></i>
           </a>
           <a
             href="https://www.linkedin.com/in/morad-elazyzy-8b12401a6/"
             target="_blank"
+            rel="noreferrer"
           >
             <i className="fa fa-linkedin"></i>
           </a>
-          <a href="https://www.behance.net/moradelazyzy" target="_blank">
+          <a
+            href="https://www.behance.net/moradelazyzy"
+            target="_blank"
+            rel="noreferrer"
+          >
             <i className="fa fa-behance-square"></i>
           </a>
         </div>
